459d48008353bda60bde285f2d3b95030773b764
CidadeAltruista <cidadealtruista@gmail.com>
2025-04-09 22:52:16 +0100

    Update form.js

--- form.js
+++ form.js
@@ -27,6 +27,8 @@ function atualizarTraducoes() {
     if (el) el.textContent = ids[id];
   }
   document.querySelector("#checkinForm button[type='submit']").textContent = t.enviar;
+  document.getElementById("cabecalho-info").innerHTML = traducoes.cabecalho[linguaAtual];
+
 }
 
 function atualizarBotoes() {
